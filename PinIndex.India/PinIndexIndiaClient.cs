e5a40ab71b1a47d9a2434613d434f5bcc1e63104
Arun <arun.prakash.456789@gmail.com>
2020-07-07 21:56:37 +0530

    documentation update

--- PinIndex.India/PinIndexIndiaClient.cs
+++ PinIndex.India/PinIndexIndiaClient.cs
@@ -17,20 +17,51 @@ namespace PinIndex.India {
 		private readonly HttpClient Client;
 		private readonly SemaphoreSlim RequestSync = new SemaphoreSlim(1, 1);
 
+		/// <summary>
+		///  Gets the total count of requests send from this instance of <see cref="PinIndexIndiaClient"/>.
+		/// </summary>
+		public int RequestsCount { get; private set; } = 0;
+
+		/// <summary>
+		/// Constructor
+		/// </summary>
+		/// <param name="clientHandler">The <see cref="HttpClientHandler"/></param>
 		public PinIndexIndiaClient(HttpClientHandler clientHandler) {
 			ClientHandler = clientHandler ?? throw new ArgumentNullException(nameof(clientHandler) + " cannot be null!");
 			Client = new HttpClient(ClientHandler, true);
 		}
 
+		/// <summary>
+		/// Constructor with default values.
+		/// </summary>
 		public PinIndexIndiaClient() {
 			ClientHandler = new HttpClientHandler();
 			Client = new HttpClient(ClientHandler, true);
 		}
 
+		/// <summary>
+		/// sync method to send a request by branch name.
+		/// </summary>
+		/// <param name="branchName">the branch name.</param>
+		/// <param name="retryCount">the maximum retry count.</param>
+		/// <returns>The response.</returns>
 		public Response RequestByBranchName(string branchName, int retryCount = MAX_RETRY_COUNT) => RequestByBranchNameAsync(branchName, default, retryCount).Result;
 
+		/// <summary>
+		/// sync method to send a request by pin code.
+		/// </summary>
+		/// <param name="pinCode">the pin code.</param>
+		/// <param name="retryCount">the maximum retry count.</param>
+		/// <returns>The response.</returns>
 		public Response RequestByPinCode(int pinCode, int retryCount = MAX_RETRY_COUNT) => RequestByPinCodeAsync(pinCode, default, retryCount).Result;
 
+		/// <summary>
+		/// async method to send a request by branch name.
+		/// </summary>
+		/// <param name="branchName">the branch name.</param>
+		/// <param name="cancellationToken">the cancellation token.</param>
+		/// <param name="retryCount">the maximum retry count.</param>
+		/// <returns>The response.</returns>
 		public async Task<Response> RequestByBranchNameAsync(string branchName, CancellationToken cancellationToken, int retryCount = MAX_RETRY_COUNT) {
 			if (string.IsNullOrEmpty(branchName)) {
 				throw new ArgumentException(nameof(branchName) + " is invalid.");
@@ -40,6 +71,13 @@ namespace PinIndex.India {
 			return await RequestAsync(requestUri, cancellationToken, retryCount).ConfigureAwait(false);
 		}
 
+		/// <summary>
+		/// async method to send a request by pin code.
+		/// </summary>
+		/// <param name="pinCode">the pin code.</param>
+		/// <param name="cancellationToken">the cancellation token.</param>
+		/// <param name="retryCount">the maximum retry count.</param>
+		/// <returns>The response.</returns>
 		public async Task<Response> RequestByPinCodeAsync(int pinCode, CancellationToken cancellationToken, int retryCount = MAX_RETRY_COUNT) {
 			if(pinCode <= 0) {
 				throw new ArgumentException(nameof(pinCode) + " is invalid.");
@@ -81,9 +119,13 @@ namespace PinIndex.India {
 			}
 			finally {
 				RequestSync.Release();
+				RequestsCount++;
 			}
 		}
 
+		/// <summary>
+		/// <inheritdoc />
+		/// </summary>
 		public void Dispose() {
 			Client?.Dispose();
 			RequestSync?.Dispose();
